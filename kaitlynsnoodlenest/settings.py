85621d5820d8dc82570a8432f853fa546cbb1c5b
Roy <roywmraziv@icloud.com>
2025-02-05 15:14:40 -0600

    Update settings.py

--- kaitlynsnoodlenest/settings.py
+++ kaitlynsnoodlenest/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-r-%0eqcja86asp_+*1@3d6t9wm65$7^p*v^m961p@)w7*3ob0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+                    'kaitlyns-noodle-nest-app-djnkv.ondigitalocean.app',
+                 ]
 
 
 # Application definition
